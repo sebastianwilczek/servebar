b5977f40a5aa9be2f8f3042a62e84ade323aa73c
Sebastian Wilczek <sebastian.wilczek@protonmail.com>
2024-08-01 00:52:46 +0200

    servebar Logic

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # servebar
-Local handlebars web page serving
+
+servebar is a server of static files with support for Handlebars templates.
+
+All files with the `.hbs` extension are served as static HTML using Handlebars. All other files are served as-is.
+
+## Installation
+
+To install servebar, run the following command:
+
+```bash
+npm install -g servebar
+```
+
+## Usage
+
+Run the following command in the directory you want to serve:
+
+```bash
+servebar
+```
+
+## Options
+
+Running `servebar` in a directory will serve the files in that directory, without further modification. Handlebars templates are rendered, and partials are included from the `partials` directory, but all variables are empty, and no helpers are included.
+
+To customize the behavior of `servebar`, you can create a `servebar` directory in the directory you want to serve. In this directory, you can create the following files:
+
+- `data.json`: A JSON file with the data to be used in the Handlebars templates.
+- `*.js`: Any JavaScript file that exports a function is mounted as a Handlebars helper. The name of the function is used as the name of the helper.
+
+### Example of `data.json`
+
+```json
+{
+  "title": "My Website",
+  "description": "This is my website."
+}
+```
+
+### Example of a helper
+
+```javascript
+const translate = (key) => {
+    return `translate("${key}")`;
+};
+
+module.exports = translate;
+```
--- index.js
+++ index.js
@@ -1,2 +1,105 @@
 #!/usr/bin/env node
-console.log("Gonna start serving Handlebars...");
+import { engine } from "express-handlebars";
+import express from "express";
+import open from "open";
+import fs from "fs";
+
+// Use current directory as the templates directory
+const templatesDir = process.cwd();
+
+const servebarDir = "servebar";
+const servebarPath = `${templatesDir}/${servebarDir}`;
+const servebarDataFileName = "data.json";
+const servebarDataFilePath = `${servebarPath}/${servebarDataFileName}`;
+const partialsDir = "partials";
+
+// Check if servebar data file exists
+const servebarDataFileExists = fs.existsSync(servebarDataFilePath);
+if (!servebarDataFileExists) {
+  console.warn("\nNo servebar data file found in the servebar directory. Handlebars templates will not be filled with data.");
+  console.warn("To serve Handlebars templates, a servebar data JSON file is required in the servebar directory.");
+  console.warn("Please create a file named 'data.json' in the 'servebar' directory.");
+}
+
+// Read data from servebar data file
+const data = servebarDataFileExists ? JSON.parse(fs.readFileSync(servebarDataFilePath)) : {};
+
+// Get all hbs files in the current directory
+const templateFiles = fs.readdirSync(templatesDir);
+const templates = templateFiles.filter((file) => file.endsWith(".hbs"));
+
+if (templates.length === 0) {
+  console.warn("\nNo Handlebars templates found in the current directory. Files will be served statically.");
+} else {
+  console.log("\nRegistering the following templates:");
+  templates.forEach((template) => console.log(`- ${template}`));
+}
+
+const app = express();
+const port = 3000;
+
+app.engine(".hbs", engine({
+  extname: "hbs",
+  partialsDir,
+  defaultLayout: false,
+}));
+app.set("view engine", ".hbs");
+app.set("views", templatesDir);
+
+(async () => {
+  let helpers = [];
+  // Check if helpers directory exists
+  if (!fs.existsSync(servebarPath)) {
+    console.warn("\nNo servebar directory found. No helpers will be registered.");
+  } else {
+    helpers = await Promise.all(
+      fs.readdirSync(servebarPath)
+        .filter((file) => file.endsWith(".js"))
+        .map(async (helper) => {
+          const { default: helperFunction } = await import(`${servebarPath}/${helper}`);
+          return helperFunction;
+        })
+      );
+  }
+
+  if (helpers.length === 0) {
+    console.warn("\nNo Handlebars helpers found in the current directory. No helpers will be registered.");
+  } else {
+    console.log("\nRegistering the following helpers:");
+  }
+
+  // Map helper functions to be functions of an object
+  const helperFunctions = {};
+  helpers.forEach((helper) => {
+    console.log(`- ${helper.name}`);
+    helperFunctions[helper.name] = helper;
+  });
+
+  const dataWithHelpers = {
+    ...data,
+    helpers: helperFunctions,
+  };
+
+  const registerView = (viewName, asIndex) => {
+    app.get(`/${asIndex ? "" : viewName}`, (req, res) => {
+      res.render(viewName, dataWithHelpers);
+    });
+  };
+
+  // Register all templates
+  templates.forEach((view) => {
+    const viewName = view.replace(".hbs", "");
+    registerView(viewName);
+    if (viewName === "index") {
+      registerView(viewName, true);
+    }
+  });
+
+  // Serve all other files statically
+  app.use(express.static(templatesDir));
+
+  app.listen(port, async () => {
+    console.log(`\nServing Handlebars at port ${port}.\n`);
+    await open(`http://localhost:${port}`);
+  });
+})();
